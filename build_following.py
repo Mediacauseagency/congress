826671a1d9e4d936c45913cf7a6e44fb7b061ed9
ericstroo <ericsstroo@gmail.com>
2014-03-01 23:27:16 -0500

    Create build_following.py
    This file calls the Twitter API to check if you are already friends with a Member and if not, follows them. Run after get_congress.py. This ran off the tweepy wrapper and requires a unique Oauth key.

---
+++ build_following.py
@@ -0,0 +1,42 @@
+### build_following.py
+import tweepy
+import json
+
+# Consumer keys and access tokens, used for OAuth
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+ 
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+ 
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
+
+#Opens the JSON file and calls it as congressPeople
+json_file = open('./congressDB/congress.txt')
+congressPeople = json.load(json_file)
+json_file.close()
+
+#Makes a list of all Congressional Twitter handles, excluding the Veep and removing all the nulls
+congressFriends= [] 
+for congressPersonFact in congressPeople ['objects']:
+	if congressPersonFact ['role_type']=='vicepresident':
+		pass
+	else:
+		congressTwit = congressPersonFact ['person']['twitterid']
+		if congressTwit != None: 
+			congressFriends.append(congressTwit)
+
+#Making a loop of the congressFriends and creating a friendship via the Tweepy wrapper	
+me='mytwitterid'
+for congressFriend in congressFriends:
+	if API.exists_friendship(me,congressFriend)==False: 
+		try:
+			api.create_friendship(congressFriend)
+		except:
+			pass
+
+
