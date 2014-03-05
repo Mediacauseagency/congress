4e7f98ba20c803c4d468058db9a0bd248dafa826
ericstroo <ericsstroo@gmail.com>
2014-03-04 23:08:28 -0500

    Update stream_and_shout.py

--- stream_and_shout.py
+++ stream_and_shout.py
@@ -6,11 +6,26 @@ import re
 from tweepy import Stream 
 from tweepy.streaming import StreamListener
 
+#Opens the JSON file and calls it as congressPeople
+json_file = open('./congressDB/congress.txt')
+congressPeople = json.load(json_file)
+json_file.close()
+
+congressFriends= [] 
+for congressPersonFact in congressPeople ['objects']:
+	if congressPersonFact ['role_type']=='vicepresident':
+		pass
+	else:
+		congressTwit = congressPersonFact ['person']['twitterid']
+		if congressTwit != None: 
+			congressFriends.append(congressTwit)
+			
 #opens the dicktionary.txt and sets it to a list
 alerts=[]
 alerts = [line.strip() for line in open("./yourlist.txt", 'r')]
 
 
+		
 # Consumer keys and access tokens, used for OAuth
 consumer_key = ''
 consumer_secret = ''
@@ -26,15 +41,19 @@ api = tweepy.API(auth)
 
 class listener(StreamListener):
 
+	#loads tweet as JSON object
+	def on_data(self, data):		
+		tweet=json.loads(data)
+		
 	#Check tweet text against the dictionary and retweet if a match is made (while preventing crash for self retweets)		
 		if tweet.has_key("text") and tweet.has_key("id"):
 			if tweet["user"]["screen_name"] in congressFriends: 
-				for phrase in dicktionary:
-						pattern = re.compile(r'\b' +phrase+ r'\b')
-						if re.search(pattern, tweet["text"].lower()):
-							print tweet
-							api.retweet(tweet["id"])
-							return True
+				for phrase in alerts:
+					pattern = re.compile(r'\b' +phrase+ r'\b')
+					if re.search(pattern, tweet["text"].lower()):
+						print tweet
+						api.retweet(tweet["id"])
+						return True
 	
 	
 	def on_error(self, status):
